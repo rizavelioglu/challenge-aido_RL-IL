f793f4047e6cf218e003f20d27e4048863f87916
Ubuntu <citec@citec.npiq52b2xr4ufeu0ovbfmwo2fh.ax.internal.cloudapp.net>
2020-02-19 21:13:57 +0000

    add trained models

--- duckietown_rl/args.py
+++ duckietown_rl/args.py
@@ -12,7 +12,7 @@ def get_ddpg_args_train():
     parser.add_argument("--batch_size", default=32, type=int)  # Batch size for both actor and critic
     parser.add_argument("--discount", default=0.99, type=float)  # Discount factor
     parser.add_argument("--tau", default=0.005, type=float)  # Target network update rate
-    parser.add_argument("--policy_noise", default=0.2, type=float)  # Noise added to target policy during critic update
+    parser.add_argument("--policy_noise", default=0.1, type=float)  # Noise added to target policy during critic update
     parser.add_argument("--noise_clip", default=0.5, type=float)  # Range to clip target policy noise
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
     parser.add_argument("--env_timesteps", default=1000, type=int)  # Frequency of delayed policy updates
