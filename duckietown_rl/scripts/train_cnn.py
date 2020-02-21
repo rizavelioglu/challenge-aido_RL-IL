7dd7c1033cecc7e145129b0a4817b53a74555a57
rizavelioglu <rzavelioglu@gmail.com>
2020-02-21 01:25:57 +0100

    change features

--- duckietown_rl/scripts/train_cnn.py
+++ duckietown_rl/scripts/train_cnn.py
@@ -3,15 +3,15 @@ import random
 import numpy as np
 import torch
 import gym
-from . import gym_duckietown
+import gym_duckietown
 import os
 
-from args import get_ddpg_args_train
-from ddpg import DDPG
-from utils import seed, evaluate_policy, ReplayBuffer
-from wrappers import NormalizeWrapper, ImgWrapper, \
+from duckietown_rl.args import get_ddpg_args_train
+from duckietown_rl.ddpg import DDPG
+from duckietown_rl.utils import seed, evaluate_policy, ReplayBuffer
+from duckietown_rl.wrappers import NormalizeWrapper, ImgWrapper, \
     DtRewardWrapper, ActionWrapper, ResizeWrapper
-from env import launch_env
+from duckietown_rl.env import launch_env
 
 policy_name = "DDPG"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
