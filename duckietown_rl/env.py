22cae04359c46b00e7ac5111803cde88abd651a9
Ubuntu <citec@citec.npiq52b2xr4ufeu0ovbfmwo2fh.ax.internal.cloudapp.net>
2020-03-01 13:42:44 +0000

    add models

--- duckietown_rl/env.py
+++ duckietown_rl/env.py
@@ -2,6 +2,6 @@ def launch_env(id=None):
     from gym_duckietown.simulator import Simulator
     env = Simulator(seed=123, map_name="zigzag_dists", max_steps=5000001, domain_rand=True, camera_width=640,
                     camera_height=480, accept_start_angle_deg=4, full_transparency=True, distortion=True,
-                    randomize_maps_on_reset=True, draw_curve=False, draw_bbox=False)
+                    randomize_maps_on_reset=False, draw_curve=False, draw_bbox=False, user_tile_start=(1,4))
 
     return env
