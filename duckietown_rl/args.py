00814f9f97d46d5ce4e3e31b706e4e1fb075e5a5
Ubuntu <citec@citec.npiq52b2xr4ufeu0ovbfmwo2fh.ax.internal.cloudapp.net>
2020-03-01 11:51:39 +0000

    remove maps

--- duckietown_rl/args.py
+++ duckietown_rl/args.py
@@ -4,9 +4,9 @@ import argparse
 def get_ddpg_args_train():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
-    parser.add_argument("--start_timesteps", default=1e6, type=int)  # How many time steps purely random policy is run for
+    parser.add_argument("--start_timesteps", default=1e0, type=int)  # How many time steps purely random policy is run for
     parser.add_argument("--eval_freq", default=5e3, type=float)  # How often (time steps) we evaluate
-    parser.add_argument("--max_timesteps", default=2e6, type=float)  # Max time steps to run environment for
+    parser.add_argument("--max_timesteps", default=1e6, type=float)  # Max time steps to run environment for
     parser.add_argument("--save_models", action="store_true", default=True)  # Whether or not models are saved
     parser.add_argument("--expl_noise", default=0.2, type=float)  # Std of Gaussian exploration noise
     parser.add_argument("--batch_size", default=32, type=int)  # Batch size for both actor and critic
