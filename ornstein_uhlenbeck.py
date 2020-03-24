7fe5c9c44723bebb8539ca63e5fa72706c223002
rizavelioglu <rzavelioglu@gmail.com>
2020-03-24 11:01:43 +0100

    add OU action noise file for testing

--- ornstein_uhlenbeck.py
+++ ornstein_uhlenbeck.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 # TODO: use action_noise.reset in --> env.reset()
 class OrnsteinUhlenbeckActionNoise():
     def __init__(self, mu, sigma, theta=.15, dt=1e-2, x0=None):
-        self.theta = theta
-        self.mu = mu
-        self.sigma = sigma
-        self.dt = dt
-        self.x0 = x0
+        self.theta = theta   # (float) the rate of mean reversion
+        self.mu = mu         # (float) the mean of the noise
+        self.sigma = sigma   # (float) the scale of the noise
+        self.dt = dt         # (float) the timestep for the noise
+        self.x0 = x0         # ([float]) the initial value for the noise output, (if None: 0)
         self.reset()
 
     def __call__(self):
@@ -38,11 +38,11 @@ def plot_graph(noise_right, noise_left, noise, timesteps, scatter=False):
     plt.ylabel("Noise")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"{noise.__repr__()}-{timesteps} steps.png")
+    # plt.savefig(f"{noise.__repr__()}-{timesteps} steps.png")
     plt.show()
 
 
-action_noise = OrnsteinUhlenbeckActionNoise(mu=np.array([0, 0]), sigma=np.array([0.5, 0.5]))
+action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(2), sigma=np.ones(2)*0.2, theta=0.7)
 noise_list = []
 timesteps = 500
 
@@ -54,4 +54,3 @@ noise_left = noise[:, 0]
 noise_right = noise[:, 1]
 
 plot_graph(noise_right, noise_left, action_noise, timesteps, scatter=False)
-
