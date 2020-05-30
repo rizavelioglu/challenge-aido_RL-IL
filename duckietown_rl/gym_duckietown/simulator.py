a93e183ce4f529ea097a75c68708814b6c3c7f66
rizavelioglu <rzavelioglu@gmail.com>
2020-05-30 17:33:00 +0200

    apply updates from original simuator

--- duckietown_rl/gym_duckietown/simulator.py
+++ duckietown_rl/gym_duckietown/simulator.py
@@ -1544,7 +1544,7 @@ class Simulator(gym.Env):
             gl.glRotatef(self.cam_angle[0], 1, 0, 0)
             gl.glRotatef(self.cam_angle[1], 0, 1, 0)
             gl.glRotatef(self.cam_angle[2], 0, 0, 1)
-            gl.glTranslatef(0, 0, self._perturb(CAMERA_FORWARD_DIST))
+            gl.glTranslatef(0, 0, CAMERA_FORWARD_DIST)
 
         if top_down:
             gl.gluLookAt(
