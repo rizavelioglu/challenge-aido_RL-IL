6f9729004793bbcb999efa075a5a7e1c8b006936
rizavelioglu <rzavelioglu@gmail.com>
2020-06-01 16:26:17 +0200

    adjust args for RL training on Colab

--- duckietown_rl/args.py
+++ duckietown_rl/args.py
@@ -8,15 +8,12 @@ def get_ddpg_args_train():
     parser.add_argument("--eval_freq", default=5e3, type=float)  # How often (time steps) we evaluate
     parser.add_argument("--max_timesteps", default=2e5, type=float)  # Max time steps to run environment for
     parser.add_argument("--save_models", action="store_true", default=True)  # Whether or not models are saved
-    parser.add_argument("--expl_noise", default=0., type=float)  # Std of Gaussian exploration noise
     parser.add_argument("--batch_size", default=32, type=int)  # Batch size for both actor and critic
     parser.add_argument("--discount", default=0.99, type=float)  # Discount factor
     parser.add_argument("--tau", default=0.005, type=float)  # Target network update rate
-    parser.add_argument("--policy_noise", default=0.2, type=float)  # Noise added to target policy during critic update
-    parser.add_argument("--noise_clip", default=0.5, type=float)  # Range to clip target policy noise
-    parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
     parser.add_argument("--env_timesteps", default=50, type=int)  # How many timesteps each episode should run for
     parser.add_argument("--replay_buffer_max_size", default=10000, type=int)  # Maximum number of steps to keep in the replay buffer
+    parser.add_argument("--train_on_colab", default=0, type=int)  # Flag whether training is on Colab
 
     return parser.parse_args()
 
